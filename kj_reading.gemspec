71552bb84e749b54e32f9c2d54b814106c2c5f79
James Robertson <digital.robertson@gmail.com>
2022-07-14 17:32:51 +0100

    improvement: If more than 1 book is found an error is now raised.

--- kj_reading.gemspec
+++ kj_reading.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'kj_reading'
-  s.version = '0.2.0'
+  s.version = '0.3.0'
   s.summary = 'A wrapper of the Kj gem. Usage: KjReading.text "mt 16, 18"'
   s.authors = ['James Robertson']
   s.files = Dir['lib/kj_reading.rb']
@@ -8,6 +8,6 @@ Gem::Specification.new do |s|
   s.signing_key = '../privatekeys/kj_reading.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/kj_reading'
 end
--- lib/kj_reading.rb
+++ lib/kj_reading.rb
@@ -5,39 +5,50 @@
 require 'kj'
 
 
+class KjReadingErr < Exception
+end
+
 class KjReading
 
   def self.text(s)
 
-    bible = Kj::Bible.new
-
     s.strip.lines.map do |line|
-            
-      r = line.match(/(?<book>\w+)\s+(?<chapter>\d+)[,\:]?\s*(?<verses>.*)/)
-      verses = r[:verses] =~ /(?:\.\.|,|-)/ ? Range.new(*r[:verses]\
-                          .split(/(?:\.\.|,|-)/).map(&:to_i)).to_a : r[:verses]
-      
-      "\n" +  bible.books(r[:book])[0].chapter(r[:chapter])\
-          .verses(verses).join(' ') + "\n" + line + "\n"
+
+      "\n" + self.verses(s).join(' ') + "\n" + line + "\n"
 
     end.join
 
   end
-  
-  def self.abbrev(s)
-    
-    Kj::Bible.new.books(s)[0].abbreviations
-    
+
+  def self.verses(s)
+
+    bible = Kj::Bible.new
+
+    r = s.match(/(?<book>\w+)\s+(?<chapter>\d+)[,\:]?\s*(?<verses>.*)/)
+    verses2 = r[:verses] =~ /(?:\.\.|,|-)/ ? Range.new(*r[:verses]\
+                        .split(/(?:\.\.|,|-)/).map(&:to_i)).to_a : r[:verses]
+
+    books = bible.books(r[:book])
+    raise KjReadingErr, 'More than 1 book found' if books.length > 1
+    books[0].chapter(r[:chapter])\
+        .verses(verses2)
+
   end
-  
+
+  def self.abbrev(s)
+
+    Kj::Bible.new.books(s)[0].abbreviations
+
+  end
+
   def self.books()
-    
+
     h = Kj::Bible.new.books.inject({}) do |r,x|
       r.merge(x.name.downcase => x.abbreviations.split(','))
     end
-    
+
   end
-  
+
   def self.check(s)
     Kj::Bible.new.books(s)[0].name
   end
@@ -48,7 +59,7 @@ end
 if __FILE__ == $0 then
 
   # e.g. KjReading.text 'mt 16, 18'
-  # e.g. KjReading.text '1co 1, 22..29'  
+  # e.g. KjReading.text '1co 1, 22..29'
 
   KjReading.text ARGV.first
 
